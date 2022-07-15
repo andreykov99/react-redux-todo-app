b3289367cd8f174f87e730c4b3e02f3cb461d374
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-07-15 13:54:16 +0300

    Add store, actions and reducers. Ref components.

--- starter/src/components/TotalCompleteItems.js
+++ starter/src/components/TotalCompleteItems.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const TotalCompleteItems = () => {
-	return <h4 className='mt-3'>Total Complete Items: 5</h4>;
+  const comletedTodos = useSelector((state) =>
+    state.todos.filter((todo) => todo.completed === true)
+  ).length;
+  return <h4 className="mt-3">Total Complete Items: {comletedTodos}</h4>;
 };
 
 export default TotalCompleteItems;
