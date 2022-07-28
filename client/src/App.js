b2e704ec5d471b6c2250700fd5d0a3ec7cf51415
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-07-28 20:22:49 +0300

    fix toast messages

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,24 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Header from './components/Header';
 
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route element={<PrivateRoute />}>
-          <Route path="/" element={<Dashboard />} />
-        </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Dashboard />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+      <ToastContainer />
+    </>
   );
 };
 
