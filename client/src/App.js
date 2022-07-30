571d5a9690389195ade1cca523f08bdd3bf3e04f
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-07-30 09:00:29 +0300

    update packages, change bootstrup import way

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,9 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Header from './components/Header';
 
-import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
