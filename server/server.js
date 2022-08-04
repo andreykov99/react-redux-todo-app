15249b8b2c94074512f1e34f61f10de46be21a31
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-08-04 21:02:03 +0300

    add errorHandler middlevare, prepare for testing

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 const colors = require('colors');
 const cors = require('cors');
 const { json } = require('body-parser');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
@@ -10,10 +11,17 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(cors());
 app.use(json());
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/todos', require('./routes/todoRoutes'));
 
+app.use(errorHandler);
+
 app.listen(port, console.log(`Server running on port ${port}`.green.bold));
+
+module.exports = app;
