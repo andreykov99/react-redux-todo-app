2287e1d481f0b08b1bf573e02a6f6f10b84e2b97
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-08-19 15:37:25 +0300

    fixed user delete if status change

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { FaUser } from 'react-icons/fa';
-import { register, reset } from '../features/auth/authSlice';
+import { register } from '../features/auth/authSlice';
 import Spinner from '../components/Spinner';
 
 const Register = () => {
@@ -31,9 +31,7 @@ const Register = () => {
     if (status === 'resolved' || user) {
       navigate('/');
     }
-
-    dispatch(reset());
-  }, [user, status, message, navigate, dispatch]);
+  }, [status, message, navigate, user, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
