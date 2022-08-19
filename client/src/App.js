93f3a122f3985abb28ae8ded6667488c059208cd
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2022-08-19 15:35:53 +0300

    add: eventBus for listen custom events - logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
+import { useDispatch } from 'react-redux';
+import { logout } from './features/auth/authSlice';
 
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
@@ -11,7 +13,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import EventBus from './common/EventBus';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  const logOut = useCallback(() => {
+    dispatch(logout());
+  }, [dispatch]);
+
+  useEffect(() => {
+    EventBus.on('logout', () => {
+      logOut();
+    });
+
+    return () => {
+      EventBus.remove('logout');
+    };
+  }, [logOut]);
+
   return (
     <>
       <Router>
